5b4bb4e69de1af2e99fcd599f3d77454ebcfe6c0
Sebastian Schmidt <mrschmidt@google.com>
2018-04-24 10:27:46 -0700

    Porting doc changes from the Web API (#178)

--- types/firestore.d.ts
+++ types/firestore.d.ts
@@ -365,20 +365,23 @@ declare namespace FirebaseFirestore {
    * An options object that configures the behavior of `set()` calls in
    * `DocumentReference`, `WriteBatch` and `Transaction`. These calls can be
    * configured to perform granular merges instead of overwriting the target
-   * documents in their entirety by providing a `SetOptions` with `merge: true`.
+   * documents in their entirety.
    */
   export interface SetOptions {
     /**
-     * Changes the behavior of a set() call to deeply merge the values specified
+     * Changes the behavior of a set() call to only replace the values specified
      * in its data argument. Fields omitted from the set() call remain
      * untouched.
      */
     readonly merge?: boolean;
 
     /**
-     * Changes the behavior of set() calls to only replace the specified
-     * field paths. All data at the specified field paths is fully replaced, 
-     * while the remaining fields remain untouched.
+     * Changes the behavior of set() calls to only replace the specified field
+     * paths. Any field path that is not specified is ignored and remains
+     * untouched.
+     *
+     * It is an error to pass a SetOptions object to a set() call that is
+     * missing a value for any of the fields specified here.
      */
     readonly mergeFields?: (string|FieldPath)[];
   }
