947bf0cadf934f87123a102a17855ed810f3ae58
Jonathan Lui <jonathanlui@google.com>
2018-09-06 13:21:45 -0700

    update synth.py to not exclude v1beta1/index.js

--- synth.py
+++ synth.py
@@ -9,20 +9,20 @@ gapic = gcp.GAPICGenerator()
 
 # tasks has two product names, and a poorly named artman yaml
 library = gapic.node_library(
-    'firestore', 'v1beta1',
-    config_path='/google/firestore/artman_firestore.yaml')
+    "firestore", "v1beta1", config_path="/google/firestore/artman_firestore.yaml"
+)
 
 # skip index, protos, package.json, and README.md
-s.copy(
-    library,
-    'dev',
-    excludes=['package.json', 'README.md', 'src/index.js',
-              'src/v1beta1/index.js'])
+s.copy(library, "dev", excludes=["package.json", "README.md", "src/index.js"])
+
+# package.json is one level deeper since firestore's src/ is under dev/
+s.replace("dev/src/v1beta1/index.js", "../../package.json", "../../../package.json")
 
 s.replace(
-    'dev/test/gapic-v1beta1.js',
-    'new firestoreModule.v1beta1.FirestoreClient\(',
-    'new firestoreModule.v1beta1(')
+    "dev/test/gapic-v1beta1.js",
+    "new firestoreModule.v1beta1.FirestoreClient\(",
+    "new firestoreModule.v1beta1(",
+)
 
 # Copy template files
 common_templates = gcp.CommonTemplates()
@@ -30,5 +30,5 @@ templates = common_templates.node_library()
 s.copy(templates)
 
 # Node.js specific cleanup
-subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'fix'])
+subprocess.run(["npm", "install"])
+subprocess.run(["npm", "run", "fix"])
