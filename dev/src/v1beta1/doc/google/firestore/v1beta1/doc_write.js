0e3f2d60fbee88df481835fc72a02e4a492fa90a
yoshi-automation <yoshi-automation@google.com>
2019-01-16 04:08:49 -0800

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- dev/src/v1beta1/doc/google/firestore/v1beta1/doc_write.js
+++ dev/src/v1beta1/doc/google/firestore/v1beta1/doc_write.js
@@ -102,6 +102,55 @@ const DocumentTransform = {
    *   The number should be among the values of [ServerValue]{@link
    * google.firestore.v1beta1.ServerValue}
    *
+   * @property {Object} increment
+   *   Adds the given value to the field's current value.
+   *
+   *   This must be an integer or a double value.
+   *   If the field is not an integer or double, or if the field does not yet
+   *   exist, the transformation will set the field to the given value.
+   *   If either of the given value or the current field value are doubles,
+   *   both values will be interpreted as doubles. Double arithmetic and
+   *   representation of double values follow IEEE 754 semantics.
+   *   If there is positive/negative integer overflow, the field is resolved
+   *   to the largest magnitude positive/negative integer.
+   *
+   *   This object should have the same structure as [Value]{@link
+   * google.firestore.v1beta1.Value}
+   *
+   * @property {Object} maximum
+   *   Sets the field to the maximum of its current value and the given value.
+   *
+   *   This must be an integer or a double value.
+   *   If the field is not an integer or double, or if the field does not yet
+   *   exist, the transformation will set the field to the given value.
+   *   If a maximum operation is applied where the field and the input value
+   *   are of mixed types (that is - one is an integer and one is a double)
+   *   the field takes on the type of the larger operand. If the operands are
+   *   equivalent (e.g. 3 and 3.0), the field does not change.
+   *   0, 0.0, and -0.0 are all zero. The maximum of a zero stored value and
+   *   zero input value is always the stored value.
+   *   The maximum of any numeric value x and NaN is NaN.
+   *
+   *   This object should have the same structure as [Value]{@link
+   * google.firestore.v1beta1.Value}
+   *
+   * @property {Object} minimum
+   *   Sets the field to the minimum of its current value and the given value.
+   *
+   *   This must be an integer or a double value.
+   *   If the field is not an integer or double, or if the field does not yet
+   *   exist, the transformation will set the field to the input value.
+   *   If a minimum operation is applied where the field and the input value
+   *   are of mixed types (that is - one is an integer and one is a double)
+   *   the field takes on the type of the smaller operand. If the operands are
+   *   equivalent (e.g. 3 and 3.0), the field does not change.
+   *   0, 0.0, and -0.0 are all zero. The minimum of a zero stored value and
+   *   zero input value is always the stored value.
+   *   The minimum of any numeric value x and NaN is NaN.
+   *
+   *   This object should have the same structure as [Value]{@link
+   * google.firestore.v1beta1.Value}
+   *
    * @property {Object} appendMissingElements
    *   Append the given elements in order if they are not already present in
    *   the current field value.
