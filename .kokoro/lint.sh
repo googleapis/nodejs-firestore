6b591d3b9ef4bbf7fccfd63d2c08a98106378e14
Sebastian Schmidt <mrschmidt@google.com>
2019-05-21 14:44:04 -0700

    Run old synth.py

--- .kokoro/lint.sh
+++ .kokoro/lint.sh
@@ -23,11 +23,9 @@ cd $(dirname $0)/..
 npm install
 
 # Install and link samples
-if [ -f samples/package.json ]; then
-  cd samples/
-  npm link ../
-  npm install
-  cd ..
-fi
+cd samples/
+npm link ../
+npm install
+cd ..
 
 npm run lint
