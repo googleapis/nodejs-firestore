20ac1530593392017bd008f03049e2d207d6db47
Lalji Kanjareeya <lalji.kanjareeya@qlogic.io>
2019-12-12 14:26:53 +0530

    fix: review changes

--- samples/solution-counters.js
+++ samples/solution-counters.js
@@ -1,38 +1,26 @@
-/*!
- * Copyright 2019 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 'use strict';
 const { Firestore, FieldValue } = require('@google-cloud/firestore');
 
 async function main() {
-  // [START create_counter]
-  async function createCounter(docRef, numShards) {
-    const colRef = docRef.collection("shards");
-    for (let count = 0; count < numShards; count++) {
-      const newDoc = colRef.doc(count.toString());
-      await newDoc.set({ count: 0 });
-    }
-  }
-  // [END create_counter]
-
   // [START increment_counter]
-  async function incrementCounter(docRef, numShards) {
+  function incrementCounter(docRef, numShards) {
     const shardId = Math.floor(Math.random() * numShards);
     const shardRef = docRef.collection("shards").doc(shardId.toString());
-    return shardRef.update({ count: FieldValue.increment(1) });
+    return shardRef.set({ count: FieldValue.increment(1) }, { merge: true });
   }
   // [END increment_counter]
 
@@ -46,27 +34,30 @@ async function main() {
   }
   // [END get_count]
 
+  // [START delete_Docs]
+  async function deleteDocs(docRef) {
+    const shardsCollectionRef = docRef.collection('shards');
+    const shardDocs = await shardsCollectionRef.get();
+    shardDocs.forEach(async (doc) => {
+      await shardsCollectionRef.doc(doc.id).delete();
+    });
+  }
+  // [END delete_Docs]
+
   // Create a new client
   const firestore = new Firestore();
   const docRef = firestore.doc('distributed_counter_samples/distributed_counter');
-  const numberOfshard = 10;
-  // Create Counter
-  await createCounter(docRef, numberOfshard);
-  console.log('counter created');
+  const numberOfShards = 10;
   // Increase the document count
-  await incrementCounter(docRef, numberOfshard);
-  console.log('counter increased');
-  // Get document count
-  let count = await getCount(docRef);
-  console.log(`new count is : ${count}`);
-  // Increase the document count
-  await incrementCounter(docRef, numberOfshard);
-  console.log('counter increased again');
-  count = await getCount(docRef);
-  console.log(`new count is : ${count}`);
-  // Delete the document
-  await docRef.delete();
-  console.log('Deleted the document');
+  incrementCounter(docRef, numberOfShards).then(async () => {
+    console.log('counter increased');
+    // Get document count
+    let count = await getCount(docRef);
+    console.log(`new count is : ${count}`);
+    // Delete the document
+    await deleteDocs(docRef);
+    console.log('Deleted the document');
+  });
 }
 
 main();
