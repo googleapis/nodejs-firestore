82273ec0035b2ddae94d8f12791f8a5c55b6560d
Sebastian Schmidt <mrschmidt@google.com>
2020-01-14 14:15:41 -0800

    fix: don't retry reads with Code ABORTED (#881)

--- dev/test/index.ts
+++ dev/test/index.ts
@@ -1065,7 +1065,7 @@ describe('getAll() method', () => {
       [Status.PERMISSION_DENIED]: 1,
       [Status.RESOURCE_EXHAUSTED]: 1,
       [Status.FAILED_PRECONDITION]: 1,
-      [Status.ABORTED]: 5,
+      [Status.ABORTED]: 1,
       [Status.OUT_OF_RANGE]: 1,
       [Status.UNIMPLEMENTED]: 1,
       [Status.INTERNAL]: 5,
