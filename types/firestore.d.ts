baa1bf47d327fc447fcf79b31af5fcceb777d36a
Brian Chen <chenbrian@google.com>
2020-02-06 16:22:39 -0800

    copy enum manually

--- types/firestore.d.ts
+++ types/firestore.d.ts
@@ -1457,7 +1457,25 @@ declare namespace FirebaseFirestore {
    * Status error code enums used by google-gax that correspond to the error
    * codes returned by Firestore gRPC calls.
    */
-  export const GrpcStatus: import('google-gax').Status;
+  export enum GrpcStatus {
+    OK = 0,
+    CANCELLED = 1,
+    UNKNOWN = 2,
+    INVALID_ARGUMENT = 3,
+    DEADLINE_EXCEEDED = 4,
+    NOT_FOUND = 5,
+    ALREADY_EXISTS = 6,
+    PERMISSION_DENIED = 7,
+    RESOURCE_EXHAUSTED = 8,
+    FAILED_PRECONDITION = 9,
+    ABORTED = 10,
+    OUT_OF_RANGE = 11,
+    UNIMPLEMENTED = 12,
+    INTERNAL = 13,
+    UNAVAILABLE = 14,
+    DATA_LOSS = 15,
+    UNAUTHENTICATED = 16
+  }
 }
 
 declare module '@google-cloud/firestore' {
