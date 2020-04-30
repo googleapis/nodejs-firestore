6aae79d17ea855d1014e965af3e23759e8b2223d
Brian Chen <chenbrian@google.com>
2020-04-30 10:41:42 -0700

    docs: Add documentation for FirestoreDataConverter (#1059)

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -162,6 +162,53 @@ const MAX_CONCURRENT_REQUESTS_PER_CLIENT = 100;
  */
 
 /**
+ * Converter used by [withConverter()]{@link Query#withConverter} to transform
+ * user objects of type T into Firestore data.
+ *
+ * Using the converter allows you to specify generic type arguments when storing
+ * and retrieving objects from Firestore.
+ *
+ * @example
+ * class Post {
+ *   constructor(readonly title: string, readonly author: string) {}
+ *
+ *   toString(): string {
+ *     return this.title + ', by ' + this.author;
+ *   }
+ * }
+ *
+ * const postConverter = {
+ *   toFirestore(post: Post): FirebaseFirestore.DocumentData {
+ *     return {title: post.title, author: post.author};
+ *   },
+ *   fromFirestore(
+ *     data: FirebaseFirestore.QueryDocumentSnapshot
+ *   ): Post {
+ *     const data = snapshot.data();
+ *     return new Post(data.title, data.author);
+ *   }
+ * };
+ *
+ * const postSnap = await Firestore()
+ *   .collection('posts')
+ *   .withConverter(postConverter)
+ *   .doc().get();
+ * const post = postSnap.data();
+ * if (post !== undefined) {
+ *   post.title; // string
+ *   post.toString(); // Should be defined
+ *   post.someNonExistentProperty; // TS error
+ * }
+ *
+ * @property {Function} toFirestore Called by the Firestore SDK to convert a
+ * custom model object of type T into a plain Javascript object (suitable for
+ * writing directly to the Firestore database).
+ * @property {Function} fromFirestore Called by the Firestore SDK to convert
+ * Firestore data into an object of type T.
+ * @typedef {Object} FirestoreDataConverter
+ */
+
+/**
  * Update data (for use with [update]{@link DocumentReference#update})
  * that contains paths (e.g. 'foo' or 'foo.baz') mapped to values. Fields that
  * contain dots reference nested fields within the document.
