006ebc7a2f3c0f60868b8011b708145d743db7fd
yoshi-automation <yoshi-automation@google.com>
2020-08-11 13:00:45 -0700

    build: use gapic-generator-typescript v1.0.7.
    This new generator will bring some changes to the generated code across all libraries, but the behavior will only change for nodejs-logging and nodejs-pubsub (those two libraries that use request batching). For other libraries, the changes should be minor (the createApiCall call is simplified) and it should be safe to merge them. Please talk to @alexander-fenster if you have any questions.
    
    PiperOrigin-RevId: 325949033
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Aug 10 21:11:13 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 94006b3cb8d2fb44703cf535da15608eed6bf7db
    Source-Link: https://github.com/googleapis/googleapis/commit/94006b3cb8d2fb44703cf535da15608eed6bf7db

--- dev/src/v1/firestore_client.ts
+++ dev/src/v1/firestore_client.ts
@@ -276,12 +276,14 @@ export class FirestoreClient {
         }
       );
 
+      const descriptor =
+        this.descriptors.page[methodName] ||
+        this.descriptors.stream[methodName] ||
+        undefined;
       const apiCall = this._gaxModule.createApiCall(
         callPromise,
         this._defaults[methodName],
-        this.descriptors.page[methodName] ||
-          this.descriptors.stream[methodName] ||
-          this.descriptors.longrunning[methodName]
+        descriptor
       );
 
       this.innerApiCalls[methodName] = apiCall;
