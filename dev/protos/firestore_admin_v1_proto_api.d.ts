7cdc69068c2bcaeff8673a8e9e2381e42fad376a
yoshi-automation <yoshi-automation@google.com>
2020-09-30 02:50:43 -0700

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- dev/protos/firestore_admin_v1_proto_api.d.ts
+++ dev/protos/firestore_admin_v1_proto_api.d.ts
@@ -1708,6 +1708,9 @@ export namespace google {
 
             /** ResourceDescriptor singular */
             singular?: (string|null);
+
+            /** ResourceDescriptor style */
+            style?: (google.api.ResourceDescriptor.Style[]|null);
         }
 
         /** Represents a ResourceDescriptor. */
@@ -1737,6 +1740,9 @@ export namespace google {
             /** ResourceDescriptor singular. */
             public singular: string;
 
+            /** ResourceDescriptor style. */
+            public style: google.api.ResourceDescriptor.Style[];
+
             /**
              * Creates a ResourceDescriptor message from a plain object. Also converts values to their respective internal types.
              * @param object Plain object
@@ -1764,6 +1770,10 @@ export namespace google {
             /** History enum. */
             type History =
                 "HISTORY_UNSPECIFIED"| "ORIGINALLY_SINGLE_PATTERN"| "FUTURE_MULTI_PATTERN";
+
+            /** Style enum. */
+            type Style =
+                "STYLE_UNSPECIFIED"| "DECLARATIVE_FRIENDLY";
         }
 
         /** Properties of a ResourceReference. */
