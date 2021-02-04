31ee3470cc168466110cae111e734194fc271ded
wu-hui <53845758+wu-hui@users.noreply.github.com>
2021-02-04 15:55:10 -0500

    Update dev/src/bundle.ts
    Co-authored-by: Sebastian Schmidt <mrschmidt@google.com>

--- dev/src/bundle.ts
+++ dev/src/bundle.ts
@@ -143,7 +143,7 @@ export class BundleBuilder {
   private elementToLengthPrefixedBuffer(
     bundleElement: firestore.IBundleElement
   ): Buffer {
-    // Convert to a valid proto message object then take its json representation.
+    // Convert to a valid proto message object then take its JSON representation.
     // This take cares of stuff like converting internal byte array fields
     // to base64 encodings.
     const message = BundleElement.fromObject(bundleElement).toJSON();
