63fa8d803ee24160d8f22493a9c42a69cc48acfc
Tom Andersen <tomandersen@google.com>
2022-09-16 16:25:09 -0400

    Comment out test

--- dev/test/count.ts
+++ dev/test/count.ts
@@ -36,19 +36,19 @@ export async function Demo2_CountOfDocumentsInACollectionWithFilter(db: Firestor
   expect(snapshot.data().count).to.equal(2000);
 }
 
-export async function Demo3_MultipleAggregations(db: Firestore) {
-  const collection = db.collection('games/halo/players');
-  const snapshot = await collection.aggregate({
-    num_players: AggregateField.count(),
-    min_age: AggregateField.min('age'),
-    score: AggregateField.sum('score'),
-  }).get();
-  const num_players: number = snapshot.data().num_players;
-  const min_age = snapshot.data().min_age ?? 0;
-  const total_points: number = snapshot.data().score ?? 0;
-  console.log(
-    `Found ${num_players} players, ` +
-    `the youngest being ${min_age} years old ` +
-    `with a total of ${total_points} points.`
-  );
-}
+// export async function Demo3_MultipleAggregations(db: Firestore) {
+//   const collection = db.collection('games/halo/players');
+//   const snapshot = await collection.aggregate({
+//     num_players: AggregateField.count(),
+//     min_age: AggregateField.min('age'),
+//     score: AggregateField.sum('score'),
+//   }).get();
+//   const num_players: number = snapshot.data().num_players;
+//   const min_age = snapshot.data().min_age ?? 0;
+//   const total_points: number = snapshot.data().score ?? 0;
+//   console.log(
+//     `Found ${num_players} players, ` +
+//     `the youngest being ${min_age} years old ` +
+//     `with a total of ${total_points} points.`
+//   );
+// }
