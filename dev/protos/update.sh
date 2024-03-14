c308aac90a506cb8177c590d7e01a258b5f198e0
Mark Duckworth <1124037+MarkDuckworth@users.noreply.github.com>
2024-03-14 12:31:43 -0600

    field_info

--- dev/protos/update.sh
+++ dev/protos/update.sh
@@ -46,7 +46,7 @@ git clone --depth 1 https://github.com/google/protobuf.git
 
 # Copy necessary protos.
 mkdir -p "${PROTOS_DIR}/google/api"
-cp googleapis/google/api/{annotations,client,field_behavior,http,launch_stage,resource}.proto \
+cp googleapis/google/api/{annotations,client,field_behavior,http,launch_stage,resource,field_info}.proto \
    "${PROTOS_DIR}/google/api/"
 
 mkdir -p "${PROTOS_DIR}/google/firestore/v1"
