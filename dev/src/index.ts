2517030b0e450f08fbb1d3776e62d6bb319dc515
Mark Duckworth <1124037+MarkDuckworth@users.noreply.github.com>
2024-07-12 12:37:33 -0600

    fix: request minified json when using the rest fallback

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -631,6 +631,7 @@ export class Firestore implements firestore.Firestore {
             {
               ...this._settings,
               fallback: useFallback,
+              minifyJson: true,
             },
             gax
           );
