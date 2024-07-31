71efa2d5b5578e688cdf26714c78553b77dfc179
Ehsan Nasiri <ehsannas@gmail.com>
2024-07-31 12:59:56 -0700

    try again.

--- dev/system-test/firestore.ts
+++ dev/system-test/firestore.ts
@@ -2832,7 +2832,7 @@ describe('Query class', () => {
     expect(snapshot.size).to.equal(100);
   });
 
-  it('supports OR queries', async () => {
+  it.only('supports OR queries', async () => {
     const collection = await testCollectionWithDocs({
       doc1: {a: 1, b: 0},
       doc2: {a: 2, b: 1},
