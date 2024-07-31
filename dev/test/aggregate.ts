a00b6e23e4a28483a5205339ee1413532108df98
Ehsan Nasiri <ehsannas@gmail.com>
2024-07-31 11:00:35 -0700

    no-op.

--- dev/test/aggregate.ts
+++ dev/test/aggregate.ts
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// comment
+
 import {expect, use} from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import {AggregateField} from '../src/aggregate';
