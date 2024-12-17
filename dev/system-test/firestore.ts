3fd0de93076adfe17aaae9e0a9b732bcae52d594
Mila <107142260+milaGGL@users.noreply.github.com>
2024-12-17 11:11:46 -0500

    Fix: sort document reference by long type id  (#2257)

--- dev/system-test/firestore.ts
+++ dev/system-test/firestore.ts
@@ -3913,6 +3913,112 @@ describe('Query class', () => {
       unsubscribe();
     });
 
+    it('snapshot listener sorts query by DocumentId same way as server', async () => {
+      const batch = firestore.batch();
+      batch.set(randomCol.doc('A'), {a: 1});
+      batch.set(randomCol.doc('a'), {a: 1});
+      batch.set(randomCol.doc('Aa'), {a: 1});
+      batch.set(randomCol.doc('7'), {a: 1});
+      batch.set(randomCol.doc('12'), {a: 1});
+      batch.set(randomCol.doc('__id7__'), {a: 1});
+      batch.set(randomCol.doc('__id12__'), {a: 1});
+      batch.set(randomCol.doc('__id-2__'), {a: 1});
+      batch.set(randomCol.doc('__id1_'), {a: 1});
+      batch.set(randomCol.doc('_id1__'), {a: 1});
+      batch.set(randomCol.doc('__id'), {a: 1});
+      // largest long number
+      batch.set(randomCol.doc('__id9223372036854775807__'), {a: 1});
+      batch.set(randomCol.doc('__id9223372036854775806__'), {a: 1});
+      // smallest long number
+      batch.set(randomCol.doc('__id-9223372036854775808__'), {a: 1});
+      batch.set(randomCol.doc('__id-9223372036854775807__'), {a: 1});
+      await batch.commit();
+
+      const query = randomCol.orderBy(FieldPath.documentId());
+      const expectedDocs = [
+        '__id-9223372036854775808__',
+        '__id-9223372036854775807__',
+        '__id-2__',
+        '__id7__',
+        '__id12__',
+        '__id9223372036854775806__',
+        '__id9223372036854775807__',
+        '12',
+        '7',
+        'A',
+        'Aa',
+        '__id',
+        '__id1_',
+        '_id1__',
+        'a',
+      ];
+
+      const getSnapshot = await query.get();
+      expect(getSnapshot.docs.map(d => d.id)).to.deep.equal(expectedDocs);
+
+      const unsubscribe = query.onSnapshot(snapshot =>
+        currentDeferred.resolve(snapshot)
+      );
+
+      const watchSnapshot = await waitForSnapshot();
+      // Compare the snapshot (including sort order) of a snapshot
+      snapshotsEqual(watchSnapshot, {
+        docs: getSnapshot.docs,
+        docChanges: getSnapshot.docChanges(),
+      });
+      unsubscribe();
+    });
+
+    it('snapshot listener sorts filtered query by DocumentId same way as server', async () => {
+      const batch = firestore.batch();
+      batch.set(randomCol.doc('A'), {a: 1});
+      batch.set(randomCol.doc('a'), {a: 1});
+      batch.set(randomCol.doc('Aa'), {a: 1});
+      batch.set(randomCol.doc('7'), {a: 1});
+      batch.set(randomCol.doc('12'), {a: 1});
+      batch.set(randomCol.doc('__id7__'), {a: 1});
+      batch.set(randomCol.doc('__id12__'), {a: 1});
+      batch.set(randomCol.doc('__id-2__'), {a: 1});
+      batch.set(randomCol.doc('__id1_'), {a: 1});
+      batch.set(randomCol.doc('_id1__'), {a: 1});
+      batch.set(randomCol.doc('__id'), {a: 1});
+      // largest long number
+      batch.set(randomCol.doc('__id9223372036854775807__'), {a: 1});
+      batch.set(randomCol.doc('__id9223372036854775806__'), {a: 1});
+      // smallest long number
+      batch.set(randomCol.doc('__id-9223372036854775808__'), {a: 1});
+      batch.set(randomCol.doc('__id-9223372036854775807__'), {a: 1});
+      await batch.commit();
+
+      const query = randomCol
+        .where(FieldPath.documentId(), '>', '__id7__')
+        .where(FieldPath.documentId(), '<=', 'A')
+        .orderBy(FieldPath.documentId());
+      const expectedDocs = [
+        '__id12__',
+        '__id9223372036854775806__',
+        '__id9223372036854775807__',
+        '12',
+        '7',
+        'A',
+      ];
+
+      const getSnapshot = await query.get();
+      expect(getSnapshot.docs.map(d => d.id)).to.deep.equal(expectedDocs);
+
+      const unsubscribe = query.onSnapshot(snapshot =>
+        currentDeferred.resolve(snapshot)
+      );
+
+      const watchSnapshot = await waitForSnapshot();
+      // Compare the snapshot (including sort order) of a snapshot
+      snapshotsEqual(watchSnapshot, {
+        docs: getSnapshot.docs,
+        docChanges: getSnapshot.docChanges(),
+      });
+      unsubscribe();
+    });
+
     it('SDK orders vector field same way as backend', async () => {
       // We validate that the SDK orders the vector field the same way as the backend
       // by comparing the sort order of vector fields from a Query.get() and
