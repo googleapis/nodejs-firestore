cf06d2f26fc5c5628f7ca780f0ad78859761bf6c
Denver Coneybeare <dconeybe@google.com>
2025-09-19 18:12:23 -0400

    Revert "REVERT ME: enable logging during tests"
    This reverts commit d501e12a9a8c9ebbf58dda2b3977258e9f082102.

--- dev/test/collection.ts
+++ dev/test/collection.ts
@@ -33,7 +33,7 @@ import {
 } from './util/helpers';
 
 // Change the argument to 'console.log' to enable debug output.
-setLogFunction(console.log);
+setLogFunction(null);
 
 describe('Collection interface', () => {
   let firestore: Firestore;
